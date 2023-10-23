8d4c4be7b4ad558e1d239239708b099d4800dfa0
MateuszEM88 <mateuszem1988@gmail.com>
2023-10-23 21:43:12 +0200

    ...

--- components/Author.jsx
+++ components/Author.jsx
@@ -4,7 +4,7 @@ const Author = ({ author }) => {
   return (
     <div className="flex w-full flex-col bg-containerBg dark:bg-containerDark  mt-8 mb-8 p-2 relative ">
       <h3 className="text-xl text-primaryText dark:text-darkText text-left mb-8 font-semibold border-b pb-4">
-        Autor
+        Autor.
       </h3>
       <h3 className=" my-4 text-lg text-primaryText dark:text-darkText font-bold">
         {author.name}
